cd72bab9d4b409f4d8f11c52b34a390d5c54faa1
Mirdukkk <vdvmirduk@gmail.com>
2021-08-05 02:20:13 +0300

    feat(replacer): replace line 4 and > escapes

--- src/replacer.js
+++ src/replacer.js
@@ -8,9 +8,14 @@ export default function replace(files, spinner) {
     spinner.text = `Rendering ${file.path}...`
     spinner.render()
 
-    const firstLine = file.content.split('\n')[0].split('/ ')
+    const fileContent = file.content.split('\n')
+
+    const firstLine = fileContent[0].split('/ ')
     const name = firstLine[firstLine.length - 1]
     const metadata = `---\ntitle: ${file.type === 'module' ? withACapital(name) : name}\n---\n\n`
+
+    const secondLine = fileContent[4]
+    file.content = file.content.replace(secondLine, '').replace(/\\>/g, '>')
     file.content = metadata + file.content
   })
 
