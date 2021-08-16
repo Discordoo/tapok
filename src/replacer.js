5a924d3b03c19cacc057191192780f2985172029
Mirdukkk <vdvmirduk@gmail.com>
2021-08-16 17:04:48 +0300

    fix: try to replace &gt/&lt to <>

--- src/replacer.js
+++ src/replacer.js
@@ -15,8 +15,10 @@ export default function replace(files, spinner) {
     const metadata = `---\ntitle: ${file.type === 'module' ? withACapital(name) : name}\n---\n\n`
 
     const secondLine = fileContent[4]
-    file.content = file.content.replace(secondLine, '')
     file.content = metadata + file.content
+      .replace(secondLine, '')
+      .replace(/(\\)?&gt/g, '>')
+      .replace(/(\\)?&lt/g, '<')
   })
 
   return files
