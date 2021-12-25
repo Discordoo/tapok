f9629d43bdfd864a7e5c2bda18ec9e7eaf8ceb75
Mirdukkk <vdvmirduk@gmail.com>
2021-12-25 23:13:30 +0300

    feat: add markdown processing

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,30 @@
 #!/usr/bin/env node
 import meow from 'meow'
 import { typedoc } from './typedoc'
-import { read } from './read'
+import { read, recursiveReaddir } from './read'
 import { generateDocs } from './docs'
 import { write } from './write'
 import { version, name } from '../package.json'
+import fs from 'fs'
+import path from 'path'
+import * as process from 'process'
 
 const help = `
 Usage:
   $ tapok path/to/typedoc-result.json
+  $ tapok path/to/markdown-contents-file.json
   
-Example:
+Flags:
+  --config -c     string    typedoc config (like typedoc.json)
+  --entry -e      string    typedoc entry point (used in typedoc --entry)
+  --markdown -m   boolean   switch to markdown files processing
+  --directory -d  string    directory where read markdown files
+  
+Examples:
   $ tapok ./docs.json -c ./typedoc-config.json -e typedoc/entry/point.ts
     ┗ will generate docs.json using typedoc and then will replace result with ddoo-compatible json file.
+  $ tapok ./markdown.json -m -d ./docs
+    ┗ will generate markdown.json that contains array of markdown files from docs directory
 `
 
 const cli = meow(help, {
@@ -26,11 +38,39 @@ const cli = meow(help, {
       alias: 'e',
       type: 'string',
       isRequired: false
+    },
+    markdown: {
+      alias: 'md',
+      type: 'boolean',
+      isRequired: flags => !!flags.directory
+    },
+    directory: {
+      alias: 'd',
+      type: 'string',
+      isRequired: flags => !!flags.markdown
     }
   }
 })
 
 if (!cli.input.length) cli.showHelp(1)
+
+if (cli.flags.m || cli.flags.markdown) {
+  console.log('Gathering all .md files from specified directory...')
+  const files = recursiveReaddir((cli.flags.directory || cli.flags.d) as string)
+
+  if (files.length) {
+    console.log(`${files.length} file${files.length === 1 ? '' : 's'} found`)
+
+    console.log('Writing json file...')
+    fs.writeFileSync(path.join(process.cwd(), cli.input[0]), JSON.stringify({
+      files, time: Date.now(), generator: `${name} ${version}`
+    }), { encoding: 'utf-8' })
+    console.log('Done')
+
+  } else console.log('There is no .md files in the specified directory')
+
+  process.exit(0)
+}
 
 console.log('Starting typedoc json generation...')
 typedoc(cli.flags.config, cli.flags.entry)
--- src/read.ts
+++ src/read.ts
@@ -1,5 +1,6 @@
 import fs from 'fs'
 import path from 'path'
+import * as process from 'process'
 
 export function read(file: string): Record<string, any> {
   const place = path.join(process.cwd(), file)
@@ -7,4 +8,34 @@ export function read(file: string): Record<string, any> {
   const content = fs.readFileSync(place, { encoding: 'utf-8' })
 
   return JSON.parse(content)
+}
+
+export function recursiveReaddir(directory: string): Array<Record<string, string>> {
+  const result: Array<Record<string, string>> = []
+
+  function loadFiles(dir = path.join(process.cwd(), directory) + '/') {
+    const files = fs.readdirSync(dir)
+    files.forEach((file) => {
+
+      if (fs.lstatSync(dir + file).isDirectory()) {
+        loadFiles(dir + file + '/')
+        return
+      }
+
+      if (file.endsWith('.md')) {
+        const content = fs.readFileSync(dir + file, { encoding: 'utf-8' })
+
+        result.push({
+          file: file.slice(0, file.length - 3),
+          dir,
+          content,
+        })
+      }
+
+    })
+  }
+
+  loadFiles()
+
+  return result
 }
